d32172d381398669629b9a6fec1db77a3c4ed81e
K. Mahesh Chandran <kmahesh110606@outlook.com>
2026-01-09 14:33:32 +0530

    data

---
+++ data.js
@@ -0,0 +1,43 @@
+export const products = [
+    {
+        id: 1,
+        name: "Comfort Plush Sneakers",
+        price: 2999,
+        rating: 4,
+        image: "images/comfyslip.jpeg",
+        discount: 10,
+        category: "Casual",
+        description: "Experience ultimate comfort with our Comfort Plush Sneakers, designed for all-day wear and style."
+    },
+    {
+        id: 2,
+        name: "Urban Runner Shoes",
+        price: 3499,
+        rating: 5,
+        image: "images/urbanrunner.jpeg",
+        discount: 15,
+        category: "Sports",
+        description: "Hit the streets with confidence in our Urban Runner Shoes, perfect for city adventures and workouts."
+    },
+]
+
+export const categories = [
+    {
+        name: "Office"
+    },
+    {
+        name: "Casual"
+    },
+    {
+        name: "Sports"
+    },
+    {
+        name: "Formal"
+    },
+    {
+        name: "Sandals"
+    },
+    {
+        name: "Boots"
+    }
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Card from "./assets/Card.jsx"
 import PaymentGateway from "./pages/PaymentGateway.jsx"
